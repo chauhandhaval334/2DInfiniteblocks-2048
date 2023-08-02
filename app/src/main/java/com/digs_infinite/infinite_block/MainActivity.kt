a5bdde614ff6b44800d1a45d121d42a3f319e013
chauhandhaval334 <chauhandhaval334@gmail.com>
2023-08-02 23:12:01 +0530

    digna commit

---
+++ app/src/main/java/com/digs_infinite/infinite_block/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.digs_infinite.infinite_block
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
